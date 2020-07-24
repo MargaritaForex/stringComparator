96be438677145231f45fcb99d12e8de78ad903ee
erika forero <ingerika.forero@gmail.com>
2020-07-24 14:55:52 -0500

    install git flow

---
+++ src/main/java/com/string/comparator/StringComparatorApplication.java
@@ -0,0 +1,13 @@
+package com.string.comparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringComparatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringComparatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/string/comparator/StringComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.string.comparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringComparatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
